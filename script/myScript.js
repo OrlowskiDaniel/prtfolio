edc2205c46c840e1275f150ae511e8f6a18c8985
OrlowskiDaniel <daniel.d.orlowski@gmail.com>
2024-12-06 09:39:53 +0100

    first version of my portfolio website

---
+++ script/myScript.js
@@ -0,0 +1,41 @@
+
+
+function showSidebar() {
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "flex"
+}
+function hideSidebar() {
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "none"
+}
+
+
+var xValues = ["HTML", "CSS", "JavaScript", "Java" ];
+var yValues = [25, 25, 25, 25 ];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+  "#e8c3b9"
+];
+
+
+new Chart("myChart", {
+  type: "pie",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "My programming languages pie chart in %"
+    }
+  }
+});
+
+
+
